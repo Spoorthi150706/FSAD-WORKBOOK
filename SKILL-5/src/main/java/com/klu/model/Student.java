3c8ecf648343dd9c6c32a8a8ff01e865f58c5e04
Spoorthi150706 <2400030718@kluniversity.in>
2026-01-22 16:50:59 +0530

    committed on 22-01-2026

---
+++ SKILL-5/src/main/java/com/klu/model/Student.java
@@ -0,0 +1,22 @@
+package com.klu.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+
+    private int id = 101;
+    private String name = "Spoorthi";
+    private String gender = "Female";
+
+    @Autowired
+    private Certification certification;
+
+    public void display() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Student Name: " + name);
+        System.out.println("Gender: " + gender);
+        certification.display();
+    }
+}
